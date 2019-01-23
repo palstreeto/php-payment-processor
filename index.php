9d0c65f2c506f57aa2fedfdad3cdca9d436acf6d
palstreeto <briangurucoder@gmail.com>
2019-01-24 00:23:45 +0300

    Create index.php
    This is a sample HTML form which collects data and POSTS to the PayMents the action URL for processing.
    It works with the

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<h2>Sample form to test PayMents Integration</h2>
+<form action="https://www.paymentprocessor-script.com/demos/downloads/Payments-New/api/webscr" method="POST">
+<input name="itemName" required="" type="text" placeholder="Enter the Item to buy" /><br /> 
+<input name="amt" readonly="readonly" type="text" value="40" /> <br />
+<input name="businessEmail" type="hidden" value="support@paymentprocessor-script.com" /> 
+<input name="currency" type="hidden" value="USD" /> 
+<input name="cancel_url" type="hidden" value="https://www.paymentprocessor-script.com/demos/downloads/Payments-New/cancel_url.php" /> 
+<input name="success_url" type="hidden" value="https://paymentprocessor-script.com/success" /> 
+<input name="notify_url" type="hidden" value="https://paymentprocessor-script.com/notify" /> 
+<input name="_xclick" type="submit" value="Buy now" /></form>
+
+</html>
